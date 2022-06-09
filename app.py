9233b9235a78c162ecc7ad4ff5f1932548203574
akhfzz <akhfzz23@gmail.com>
2022-06-09 21:44:02 +0700

    'fix-predict'

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import tensorflow as tf
 import keras as K
 from keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
 from pickle import load
 import numpy as np
 import joblib
@@ -38,35 +39,32 @@ def hello():
 @app.route("/predict", methods=["GET","POST"])
 def predict():
     data = {"success": False}
+    x = None
 
     params = flask.request.json
+
     if (params == None):
         params = flask.request.args
 
     # if parameters are found, return a prediction
     if (params != None):
         x=pd.DataFrame.from_dict(params, orient='index').transpose()
-        y = x[["product_score", "qty", "freight_price", 
-            "product_weight_g", "lag_price", "comp_1", "ps1", "fp1", "comp_2", "ps2","fp2"]]
-        # with graph.as_default():
-        nilai = np.array([[  3.7      ,  16.0       ,  15.628125 ,  35.0       , 850.0       ,
-         35.0       , 103.2333333,   4.1      ,  22.3      ,  35.0       ,
-          3.7      ,  15.628125 ,   0       ,   0       ,   0       ,
-          0       ,   1       ,   0       ,   0       ,   0      ,
-          0      ]])
-        sc = joblib.load('ml_models/repfit_scaler.joblib')
 
-        prediction_result = str(model.predict(x)[0][0])
-        hasil_scale = sc.transform(nilai)
+    nilai = np.array(x.values)
+
+    scaler_load = joblib.load('ml_models/scaler_v2.save')
+
+    hasil_scale = scaler_load.transform(nilai)
+    prediction_result = str(model.predict(hasil_scale)[0][0])
         
         # this is used to get the price from prediction result (unscaled)
         ## unscaled = scaled * (max - min) + min ......(1)
         # which is in this case
         ## your_price = prediction_result * (max - min) + min
         ### max and min values are the constants
-        data["your_price"] = (float(prediction_result) * (364.900000 - 19.900000) + 19.900000)
-        data["success"] = True
-        data["hasil_scale"] = str(hasil_scale)
+    data["your_price"] = (float(prediction_result) * (364.900000 - 19.900000) + 19.900000)
+    data["success"] = True
+    data["hasil_scale"] = str(hasil_scale)
 
     # return a response in json format 
     return flask.jsonify(data)    
